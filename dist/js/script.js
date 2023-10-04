87917fe9fc0e8b27d09e370a7dc955a25e4944d0
garted <garted93@gmail.com>
2023-10-04 18:38:19 +0300

    percent

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,15 @@
+const burger = document.querySelector(".hamburger");
+const overlay = document.querySelector(".overlay");
+const cross = document.querySelector(".cross");
+const socials = document.querySelector(".socials");
+const promo = document.querySelector(".promo");
 
+burger.addEventListener("click", () => {
+  overlay.style.left = "0";
+  overlay.style.top = "0";
+});
+
+cross.addEventListener("click", () => {
+  overlay.style.left = "-100%";
+  overlay.style.top = "-100%";
+});
